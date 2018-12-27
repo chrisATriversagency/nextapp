0db80d1cc85c302369aacf73b1b6b74ab2bd19d8
Chris Calascione <ccalascione@riversagency.com>
2018-12-27 17:11:15 -0500

    create page title component

---
+++ components/PageTitle.js
@@ -0,0 +1,8 @@
+const PageTitle = (props) => (
+    <div>
+        <h1>{props.title}</h1>
+        <p>{props.copy}</p>
+    </div>
+);
+
+export default PageTitle;
--- pages/support.js
+++ pages/support.js
@@ -1,9 +1,10 @@
 import Header from '../components/Header';
 import Layout from '../components/Layout';
+import PageTitle from '../components/PageTitle';
 
 const Support = () => (
     <Layout>
-        <p>This is the support page</p>
+        <PageTitle title="Support" copy="Cras diam elit, tempus id egestas a, eleifend et nunc. Proin ipsum risus, lobortis a purus quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
     </Layout>
 );
 
