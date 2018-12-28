9ede0b071ae2cd79a36b1dfecb825aa72f77e942
Chris Calascione <ccalascione@riversagency.com>
2018-12-28 16:23:02 -0500

    updates for system overview page to load properly, create resources page

---
+++ pages/resources.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+import PageTitle from '../components/PageTitle';
+
+const Resources = () => (
+    <Layout>
+        <PageTitle title="Resources" copy="Cras diam elit, tempus id egestas a, eleifend et nunc. Proin ipsum risus, lobortis a purus quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
+        <div>
+        </div>
+    </Layout>
+);
+
+export default Resources;
--- pages/system-overview.js
+++ pages/system-overview.js
@@ -1,26 +1,23 @@
 import Layout from '../components/Layout';
 import StepLink from '../components/StepLink';
+import PageTitle from '../components/PageTitle';
 
 import Link from 'next/link';
 
 import steps from '../data/steps';
 
-const SystemOverview = (props) => (
-
-    return (
-        <Layout>
-            <h1>A Blueprint for Implementation and Improvement of CMM</h1>
-            <p>A step-by-step guide to facilitate quality implementation and improvement of CMM that is grounded in implementation science theory and refined with evidence-based learnings from the CMM in Primary Care Study.</p>
-            <ul>
-                {steps.map((step) => (
-                    <StepLink
-                        step={step}
-                        key={step.number}
-                    />
-                ))}
-            </ul>
-        </Layout>
-    )
+const SystemOverview = props => (
+    <Layout>
+        <PageTitle title="A Blueprint for Implementation and Improvement of CMM" copy="A step-by-step guide to facilitate quality implementation and improvement of CMM that is grounded in implementation science theory and refined with evidence-based learnings from the CMM in Primary Care Study." />
+        <ul>
+            {steps.map((step) => (
+                <StepLink
+                    step={step}
+                    key={step.number}
+                />
+            ))}
+        </ul>
+    </Layout>
 );
 
 export default SystemOverview;
